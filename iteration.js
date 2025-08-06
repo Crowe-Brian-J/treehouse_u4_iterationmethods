2fde73c388c1efed3eaeb852f8a734b62ad8732d
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 22:58:07 -0400

    iterate over fruits array to capitalize strings

--- iteration.js
+++ iteration.js
@@ -1,1 +1,16 @@
+//given array
 const fruits = ['apple', 'pear', 'cherry']
+
+//create capitalized fruits array
+let capitalizedFruits = []
+
+//iterate over fruits to capitalize fruits and push to newly created array
+fruits.forEach((fruit) => {
+  //singular
+  let capitalizedFruit = fruit.toUpperCase()
+  //push to array
+  capitalizedFruits.push(capitalizedFruit)
+})
+
+//log for posterity's sake
+console.log(capitalizedFruits)
