4d1475807bef48b64f258db0edf3483af5f0df14
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 23:48:56 -0400

    map iteration over fruits to return capitalized fruit words

--- iteration.js
+++ iteration.js
@@ -71,3 +71,14 @@ console.log(evens) */
 /* const strings = ['1', '2', '3', '4', '5']
 const number = strings.map((string) => parseInt(string, 10))
 console.log(number) */
+
+//fruits.toUpperCase() challenge with map method
+
+//given array
+const fruits = ['apple', 'pear', 'cherry']
+//map over array into new array
+const capitalizedFruits = fruits.map((fruit) => {
+  return fruit.toUpperCase()
+})
+
+console.log(capitalizedFruits)
